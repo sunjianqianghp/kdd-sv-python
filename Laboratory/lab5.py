ef4b2d710baea4852a7337dc484ea868b349c13f
sunjianqiang <15921915886@163.com>
2019-08-11 18:14:06 +0800

    cg

--- Laboratory/lab5.py
+++ Laboratory/lab5.py
@@ -1,18 +1,48 @@
+# -*- coding: utf-8 -*-
 import numpy as np
+from sklearn import svm
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
-plt.rcParams['font.sans-serif'] = ['SimHei'] # 指定默认字体
-plt.rcParams['axes.unicode_minus'] = False # 解决保存图像是负号'-'显示为方块的问题
-X = []
-Y = []
-learning_rate=1
-global_steps=3000
-decay_steps=50
-decay_rate=0.9
-# 指数学习率衰减过程
-for global_step in range(global_steps):
-    decayed_learning_rate = learning_rate * decay_rate**(global_step / decay_steps)
-    X.append(global_step / decay_steps)
-    Y.append(decayed_learning_rate)
-    #print("global step: %d, learning rate: %f" % (global_step,decayed_learning_rate))
-print(X[0:100])
-print(Y[0:100])
+
+data = []
+labels = []
+with open("svm_data.txt") as ifile:
+    for line in ifile.readlines():
+        tokens = line.strip().split("\t")
+        try:
+            data.append([float(tk) for tk in tokens[:-1]])
+            labels.append(int(tokens[-1]))
+        except Exception:
+            print("wrong")
+
+
+x = np.array(data)
+labels = np.array(labels)
+y = np.zeros(labels.shape)
+y[labels == 1] = 1
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.3)
+
+h = 0.1
+x_min, x_max = x_train[:, 0].min() - 0.1, x_train[:,0].max() + 0.1
+y_min, y_max = x_train[:, 1].min()  -   1, x_train[:, 1].max() +   1
+xx, yy = np.meshgrid( np.arange(x_min, x_max, h),
+                               np.arange(y_min, y_max, h))
+titles = ['LinearSVC(linear lernel)',
+           'SVC with RBF kernel',
+           'SVC with Sigmoid kernel']
+clf_linear = svm.SVC(kernel = 'linear').fit(x, y)
+clf_rbf    = svm.SVC(gamma = 'auto').fit(x, y)
+clf_sigmoid = svm.SVC(kernel = 'sigmoid').fit(x, y)
+
+for i, clf in enumerate((clf_linear, clf_rbf, clf_sigmoid)):
+    answer = clf.predict( np.c_[xx.ravel(), yy.ravel()] )
+    plt.subplot(2,2,i+1)
+    z = answer.reshape(xx.shape)
+    plt.contourf(xx, yy, z, 2, alpha = 0.8)
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.xticks(())
+    plt.yticks(())
+#     plt.titile(titles[i])
+plt.show()
